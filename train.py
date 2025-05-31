5a948523f12743b4fa5a0f213e0d3e3032e20186
Mohamed Amine (T-Rex) TOURHAM <93058453+mtourham@users.noreply.github.com>
2025-05-31 16:30:50 +0000

    starting points set on trainer

--- train.py
+++ train.py
@@ -1,10 +1,32 @@
 import json
+import csv
+
+x = []
+y = []
 
 with open('thetas.json', 'r') as file:
     thetas = json.load(file)
+
+with open('data.csv', 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        x.append(float(row['km']))
+        y.append(float(row['price']))
 
 # Access the values
 theta0 = thetas['theta0']
 theta1 = thetas['theta1']
 
-print(theta0, theta1, thetas)
+learing_rate = 0.1
+
+iterations = 1000
+
+sum_error0 = 0
+sum_error1 = 0
+
+predictions = []
+
+for i,kms in enumerate(x):
+    predictions.append(theta0 + (theta1 * kms)
+    if predictions[i] != y[i]:
+        
